3a98f79b2c50fc3ff670b310271d74d30d20c761
Martin Fetzel <martin.fetzel@get-the-solution.net>
2020-11-10 20:11:04 +0100

    experimenting

--- Blazor.Console/BlazorConsole.cs
+++ Blazor.Console/BlazorConsole.cs
@@ -3,80 +3,143 @@ using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Blazor.Console
 {
-    public partial class BlazorConsoleComponent : ComponentBase
+    public partial class BlazorConsoleComponent : ComponentBase, IDisposable
     {
         private readonly StringBuilder _StringBuilder = new StringBuilder();
         protected ConsoleInput Command { get; set; }
         protected string Output = string.Empty;
-        protected string Placeholder { get; set; } = "Wait for input request.";
-        protected string Disabled { get; set; } = DisabledString;
+        protected string Placeholder { get; set; } //= "Wait for input request.";
+        protected string Disabled { get; set; } = null;// DisabledString;
         public static readonly string DisabledString = "Disabled";
+        public event EventHandler<string> CommandInputEvent;
+
         [Parameter] public string Name { get; set; }
         public string Version() => System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
         protected StringWriterRedirect _stringWriterRedirect;
         protected StringReaderRedirect _stringReaderRedirect;
+        private Timer _Timer;
+        protected override void OnInitialized()
+        {
+            //refresh ui every 800ms
+            _Timer = new Timer(_ => InvokeAsync(StateHasChanged), null, 800, 800);
+            CommandInputEvent += BlazorConsoleComponent_CommandInputEvent;
+            base.OnInitialized();
+        }
+
         protected override Task OnInitializedAsync()
         {
             _stringWriterRedirect = new StringWriterRedirect() { OnWrite = WriteLine };
             System.Console.SetOut(_stringWriterRedirect);
-            _stringReaderRedirect = new StringReaderRedirect(Read);
+            _stringReaderRedirect = new StringReaderRedirect(ReadAsync, Read);
             System.Console.SetIn(_stringReaderRedirect);
 
             Command = new ConsoleInput();
-            Disabled = DisabledString;
 
             return base.OnInitializedAsync();
         }
-        TaskCompletionSource<string> StringReadTaskCompletionSource = new TaskCompletionSource<string>();
-        public Task<String> Read()
+        private bool RecievedInput = false;
+        private void BlazorConsoleComponent_CommandInputEvent(object sender, string e)
         {
-            DisableInput();
-
-
-            ////StringReadTaskCompletionSource = new TaskCompletionSource<string>();
-            ////var t = StringReadTaskCompletionSource.Task.ConfigureAwait(false);
-
-            //while (String.IsNullOrEmpty(Command.Text))
-            //{
-            //    if (!String.IsNullOrEmpty(Command.Text))
-            //    {
-            //        break;
-            //    }
-            //    //System.Threading.Thread.Yield();
-            //}
-
-
-            ////string r = t.GetAwaiter().GetResult();
-            //Disabled = DisabledString;
-            //StateHasChanged();
-            ////return r;
-            return Task.FromResult("oh");
+            //event will be fired on user input. the user input should be returned by the Read function
+            RecievedInput = true;
         }
-        
+        private ManualResetEvent manualResetEvent;
+        public string Read()
+        {
+            manualResetEvent = new ManualResetEvent(false);
+            
+            BackgroundWorker backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.RunWorkerAsync();
+            manualResetEvent.WaitOne(1000);
 
+            //wait until user enterted command and return value.
+            return "ok";
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private string ReadPrivate()
+        {
+            return "ok";
+        }
+        public async Task<string> ReadAsync()
+        {
+            string result = "";
+            //await Task.Yield(); monitor exception
+            await InvokeAsync(async () =>
+            {
+                //await Task.Yield();
+                result = "muh";
+            });
+            return result;
+        }
+        private async Task<string> ReadPrivateAsync()
+        {
+            this.RecievedInput = false;
+            string result = "";
+            await InvokeAsync(async () =>
+            {
+                do
+                {
+                    result = "okfromloop";
+                } while (this.RecievedInput);
+            });
+            return result;
+        }
+
+
+        //public async Task<String> ReadAsync()
+        //{
+        //    await InvokeAsync(ReadAsync);
+
+        //    //await Task.Yield();
+        //    ////DisableInput();
+
+        //    //await InvokeAsync(() => Task.Delay(800));
+
+        //    //////string r = t.GetAwaiter().GetResult();
+        //    ////Disabled = DisabledString;
+        //    ////StateHasChanged();
+        //    //////return r;
+        //    //return Command.Text + "1";
+
+        //}
+
+        protected void OnCommandInputEvent(string inputCommand)
+        {
+            CommandInputEvent?.Invoke(this, inputCommand);
+        }
         protected Task Execute(EditContext context)
         {
             if (context?.Model is ConsoleInput consoleInput)
             {
-                //StringReadTaskCompletionSource.SetResult(consoleInput.Text);
+                OnCommandInputEvent(consoleInput.Text);
                 consoleInput.Text = string.Empty;
             }
             return Task.CompletedTask;
         }
-
         public Task WriteLine(string consoleInput)
+        {
+            return InvokeAsync(() => WriteLinePrivate(consoleInput));
+        }
+        private Task WriteLinePrivate(string consoleInput)
         {
             string readLineText = consoleInput;
             _StringBuilder.AppendLine($"<br>{readLineText}");
             Output = _StringBuilder.ToString();
-            DisableInput();
+            //DisableInput();
             //force rerender of component
             StateHasChanged();
             return Task.CompletedTask;
@@ -103,6 +166,12 @@ namespace Blazor.Console
         {
             Disabled = DisabledString;
             Placeholder = "Wait for input request.";
+        }
+
+        public void Dispose()
+        {
+            _Timer?.Dispose();
+            _Timer = null;
         }
     }
 }
--- Blazor.Console/StringReaderRedirect.cs
+++ Blazor.Console/StringReaderRedirect.cs
@@ -2,29 +2,41 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Blazor.Console
 {
     public class StringReaderRedirect : StringReader
     {
-        private readonly Func<Task<string>> _ReadRedirectFunc;
-        public StringReaderRedirect(Func<Task<string>> readredirect) : base("foo")
+        private readonly Func<Task<string>> _ReadRedirectTaskFunc;
+        private readonly Func<string> _ReadRedirectFunc;
+        public StringReaderRedirect(Func<Task<string>> readredirect, Func<string> readTask) : base("foo")
         {
-            _ReadRedirectFunc = readredirect;
+            _ReadRedirectTaskFunc = readredirect;
+            _ReadRedirectFunc = readTask;
         }
 
         public override string ReadLine()
         {
-            //return _ReadRedirectFunc?.Invoke();
-            //return base.ReadLine();
-            Task<string> task = _ReadRedirectFunc?.Invoke();
+            //Task.Yield();
+            if(_ReadRedirectFunc!=null)
+            {
+                return _ReadRedirectFunc.Invoke();
+            }
+            else
+            {
+                Task<string> task = _ReadRedirectTaskFunc?.Invoke();
+                return task?.ConfigureAwait(false).GetAwaiter().GetResult();
+            }
 
-            return task?.GetAwaiter().GetResult();
-        }
-        public Task<string> GetString()
-        {
-            return Task.FromResult("okay);");
+            //return base.ReadLine();
+
+            //return _ReadRedirectFunc?.Invoke();
+
+
+
+            //return "asdf";
         }
         public override int Read()
         {
