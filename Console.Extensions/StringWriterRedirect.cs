b6173bfed0731f9e9a1c399e34168340eff3ebfb
Martin Fetzel <U01K1RM@post.at>
2020-11-13 15:06:14 +0100

    fix nullref

--- Console.Extensions/StringWriterRedirect.cs
+++ Console.Extensions/StringWriterRedirect.cs
@@ -12,10 +12,14 @@ namespace Console.Extensions
     /// </remarks>
     public class StringWriterRedirect : StringWriter
     {
-        public Func<String, Task> StringWriterLineRedirectTaskFunc;
-        public Func<String, Task> StringWriterRedirectTaskFunc;
+        public Func<String, Task>? StringWriterLineRedirectTaskFunc;
+        public Func<String, Task>? StringWriterRedirectTaskFunc;
 
-        private Task WriteGeneric<T>(T value) { return StringWriterRedirectTaskFunc?.Invoke(value.ToString()); }
+        private Task WriteGeneric<T>(T value)
+        {
+            if (value == null) return Task.CompletedTask;
+            return StringWriterRedirectTaskFunc?.Invoke(value.ToString());
+        }
 
         public override async void Write(char value) { await WriteGeneric<char>(value); }
         public override async void Write(string value) { await WriteGeneric<string>(value); }
@@ -24,7 +28,11 @@ namespace Console.Extensions
         public override async void Write(double value) { await WriteGeneric<double>(value); }
         public override async void Write(long value) { await WriteGeneric<long>(value); }
 
-        private Task WriteLineGeneric<T>(T value) { return StringWriterLineRedirectTaskFunc?.Invoke(value.ToString()) ?? Task.CompletedTask; }
+        private Task WriteLineGeneric<T>(T value)
+        {
+            if (value == null) return Task.CompletedTask;
+            return StringWriterLineRedirectTaskFunc?.Invoke(value.ToString());
+        }
         public override async void WriteLine(char value) { await WriteLineGeneric<char>(value); }
         public override async void WriteLine(string value) { await WriteLineGeneric<string>(value); }
         public override async void WriteLine(bool value) { await WriteLineGeneric<bool>(value); }
